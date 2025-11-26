a2d36881f5bb2cbd8eb79f0f836cb8b52e76d800
arjunshirsat <arjunsyt11@gmail.com>
2025-11-26 16:35:49 +0530

    initail

---
+++ src/test/java/com/aivulfix/VulanfixissueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivulfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulanfixissueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
